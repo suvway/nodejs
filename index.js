bd9f43b8207a39588e94fb93a6fbddefb1a7d2d2
suvway <suvway@gmail.com>
2021-01-27 23:14:41 +0900

    login and token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');//body-Parser 라이브러리 불러오기
+const cookieParser = require('cookie-parser');//cookie-Parser import
 const { User } =require("./models/User");//User Model을 불러온다.
 
 const config = require('./config/key');
@@ -10,7 +11,8 @@ const config = require('./config/key');
 app.use(bodyParser.urlencoded({extended:true}));
 
 //application/json을 분석해서 가져오게 해준다.
-app.use(bodyParser.json());
+app.use(bodyParser.json());//bodyparser로 body를 json형식으로 parsing
+app.use(cookieParser());//cookie를 json으로 parsing
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -22,13 +24,10 @@ mongoose.connect(config.mongoURI,{
 app.get('/', (req, res) => res.send('Hello World!~~안녕하세요 nodemon 실행중'))
 
 
-app.post('/register',(req, res)=>{
-
+app.post('/register',(req, res) => {
     //회원 가입 할때 필요한 정보를 clinet에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
     //req.body 안에 { id:"hello" password:1234}로 body에 들어 있는 것을 body parser를 통해서 가져온다.
-
-
     const user = new User(req.body)
 
     user.save((err, doc) => {
@@ -39,5 +38,37 @@ app.post('/register',(req, res)=>{
     })
 })
 
+app.post('/login', (req, res) => {
+    //DB에서 요청된 Email이 있는지 찾는다.
+    
+    User.findOne({email: req.body.email}, (err,user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "요청한 이메일과 일치하는 유저가 없습니다."
+            })
+        }
+        //DB에 있는 Email과 요청된 비밀번호가 일치하는지 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {//comparePassword는 userSchema에 등록한 function
+            if(!isMatch){
+            return res.json({
+                loginSuccess: false, 
+                message: "비밀번호가 틀렸습니다."
+            })
+        }
+            //비밀번호가 일치하면 토큰을 생성
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                //토큰을 저장한다. 어디에? cookie, localStorage, session 
+                //어디에 저장하는지는 논란이 있고 방법이 많다. 각기 다 장단점이 있다.
+                res.cookie("x_auth", user.token)//cookie에 x_auth로 token저장
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+
+            })
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;//salt를 몇글자로 만들지 설정
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,48 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+//유저 모델에 유저정보를 저장하기 전에 즉 post방식으로 받은 정보를 mongoDB에 넣기 전에
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        //비밀 번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)//next는 index.js에 있는 user.save 
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash //user.password를 salt를 이용해서 만든 hash로 바꾼다.
+                next()
+            })
+        })
+    } else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainpassword 1234567 암호화된 비밀번호 $2b$10$nbG.WYuKivFLfStpDYfCoOBM1.r60pcMKRedHkS6rDHgvR6GFLVXO plainpassword를 암호화해서 비교해야한다
+    //복호화불가능
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){//bcrypt가 plainPassword를 암호화해서 this.password와 비교
+        if(err) return cb(err);
+        cb(null, isMatch)//isMatch is true
+    })
+}
+
+userSchema.methods.generateToken = function(cb){//parameter로 callback함수를 받는다.
+
+    var user = this;
+    //jsonwebtoken으로 token 생성하기
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')//_id는 mongoDB에 저장된 PK _id값 //_id값이 plainObject가 아니라서 tohexString으로 변환
+    //user.id + 'secretToken' = token
+    //-> 'secretToken' ->user._id
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)//model(User)로 schema를 감싸서 user를 설정한다.
 
